91b1d27e2fdbe9fb20ec9556571649df850dc545
Felicia <feelicianorman@hotmmail.com>
2024-12-28 23:30:26 +0100

    added animation to subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,15 +24,23 @@ export default function Home() {
               <h1 className={`${styles.title} ${museoModerno.variable}`}>
                   Hi! I&apos;m Felicia Norman. <br></br>
               </h1>
-              <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>Developer.</h1>
+              <div className={`${styles.words} ${styles.rotate}`}>
+                  <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>Developer.</h1>
+                  <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>Designer.</h1>
+                  <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>Problem Solver.</h1>
+                  <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>CSS Ninja.</h1>
+                  <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>
+                      Stack Overflow Navigator.
+                  </h1>
+              </div>
               <div className={styles.svgContainer}>
                   <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height="24px"
                       viewBox="0 -960 960 960"
                       width="24px"
-            fill="#ffffff"
-            className={styles.svg}
+                      fill="#ffffff"
+                      className={styles.svg}
                   >
                       <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
                   </svg>
