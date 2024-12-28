634dfc676049279f2d074243872be4e25339e9a4
Felicia <feelicianorman@hotmmail.com>
2024-12-28 18:55:10 +0100

    more work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,29 +24,21 @@ export default function Home() {
               <h1 className={`${styles.title} ${museoModerno.variable}`}>
                   Hi! I&apos;m Felicia Norman. <br></br>
               </h1>
-              <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>
-                  Frontend Developer.{" "}
-                  <div className={styles.svgContainer}>
-                      <svg
-                          width="800px"
-                          height="800px"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className={styles.svg}
-                      >
-                          <path
-                              d="M15.7 4C18.87 4 21 6.98 21 9.76C21 15.39 12.16 20 12 20C11.84 20 3 15.39 3 9.76C3 6.98 5.13 4 8.3 4C10.12 4 11.31 4.91 12 5.71C12.69 4.91 13.88 4 15.7 4Z"
-                              stroke="#fff"
-                              stroke-width="2"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                          />
-                      </svg>
-                  </div>
-              </h1>
-       
+              <h1 className={`${styles.subTitle} ${museoModerno.variable}`}>Developer.</h1>
+              <div className={styles.svgContainer}>
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+            fill="#ffffff"
+            className={styles.svg}
+                  >
+                      <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+                  </svg>
+              </div>
           </div>
       </main>
   );
 }
+/* https://codepen.io/elfick/pen/rQYEjg*/
