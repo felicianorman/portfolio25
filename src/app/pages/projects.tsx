eb1fa5bffddbac529777299fa84338dad692c3e0
Felicia <feelicianorman@hotmmail.com>
2024-12-30 16:53:48 +0100

    added imgs

--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -1,6 +1,6 @@
 import styles from '@/app/ui/projects.module.css';
 import clsx from "clsx";
-import { MuseoModerno } from "next/font/google";
+import { MuseoModerno, Poppins } from "next/font/google";
 import { useEffect, useState } from 'react';
 import { fetchRepos } from '../lib/api';
 // import { fetchRepos } from '../lib/api';
@@ -9,6 +9,12 @@ const museoModerno = MuseoModerno({
     variable: "--font-museo-moderno",
     subsets: ["latin"],
     weight: ["400", "500"],
+});
+
+const poppins = Poppins({
+    variable: "--font-poppins",
+    subsets: ["latin"],
+    weight: ["500", "600"],
 });
 
 interface ProjectsProps {
@@ -32,20 +38,20 @@ export default function Projects({ className }: ProjectsProps) {
         loadRepos();
     }, []);
 
+    const filteredRepos = repos.filter((repo) => repo.description);
 
     return (
         <section className={clsx(styles.projects, className)}>
             <h2 className={`${museoModerno.variable}`}>projects</h2>
-            <ul>
-                {repos.map((repo) => (
-                    <li key={repo.id}>
-                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                            {repo.name}
-                        </a>
+            <div className={styles.projectList}>
+                {filteredRepos.map((repo) => (
+                    <article key={repo.id} className={`${styles.project} ${poppins.variable}`}>
+                        <h3>{repo.name}</h3>
+                        
                         {repo.description && <p>{repo.description}</p>}
-                    </li>
+                    </article>
                 ))}
-            </ul>
+            </div>
         </section>
     );
 }
